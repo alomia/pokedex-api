23cb43303329c5e89a9d1e9b378732344e1f1d18
Alejandro Alomia <alejandroalomia730@gmail.com>
2026-06-23 15:46:51 -0500

    fix: resolve Railway deployment issues in app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,22 +5,25 @@ import { ConfigModule } from "@nestjs/config";
 
 import { join } from "path";
 import { PokemonModule } from "./pokemon/pokemon.module";
-import { CommonModule } from './common/common.module';
-import { SeedModule } from './seed/seed.module';
+import { CommonModule } from "./common/common.module";
+import { SeedModule } from "./seed/seed.module";
 import { EnvConfiguration } from "./config/app.config";
 import { JoiValidationSchema } from "./config/joi.validation";
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      load: [EnvConfiguration],  
-      validationSchema: JoiValidationSchema
+      load: [EnvConfiguration],
+      validationSchema: JoiValidationSchema,
     }),
+
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, "..", "public"),
+      exclude: ["/api/(.*)"],
     }),
 
     MongooseModule.forRoot(process.env.MONGODB_URI!),
+    
     PokemonModule,
     CommonModule,
     SeedModule,
